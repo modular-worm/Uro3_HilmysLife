ecbdaf0d2c85c6640a9e83beacff2e1a6ce9a026
modular-worm <59914510+modular-worm@users.noreply.github.com>
2020-02-02 08:52:27 -0800

    Frederick Ebenezer Leonard-16919378-menambah kondisi mati(game over), dan konsequensi jika status dibawah nol (implementasi control state machine)

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 //Mohon maaf belum dapat mengembangkan versi OOP kak, Karena masih dalam proses belajar
 //Versi yang menerapkan ADT, Pointer, dan beberapa hal lain akan segera dikembangkan
 //Status
-int hP = 50, xP = 0, nW = 50000,fP = 50, sP= 10, hgn = 50, lvl = 0;
+int hP = 50, xP = 0, nW = 50000,fP = 50, sP= 10, hgn = 50, lvl = 0, jam = 0, miskin = 1;;
+
 
 
 int aksi(char act){
@@ -13,6 +14,7 @@ int aksi(char act){
         hP = hP + 15;
         nW = nW - 15000;
         fP = fP + 10;
+        jam = jam + 1;
         printf("Hilmy makan telor.\n");
 
     }
@@ -20,6 +22,7 @@ int aksi(char act){
         hP = hP+5;
         fP = fP+5;
         hgn = hgn - 10;
+        jam = jam + 6;
         printf("Hilmy bermimpi indah.\n");
     }
     else if(act=='C'){
@@ -27,6 +30,7 @@ int aksi(char act){
         hP = hP - 15;
         sP = sP - 35;
         hgn = hgn - 10;
+        jam = jam + 5;
         printf("Hilmy sekarang berbau bawang.\n");
     }
     else if(act=='D'){
@@ -34,6 +38,7 @@ int aksi(char act){
         xP = xP + 5;
         fP = fP + 20;
         sP = sP + 15;
+        jam = jam + 1;
         printf("Hilmy beribadah.\n");
     }
     else if(act=='E'){
@@ -42,6 +47,7 @@ int aksi(char act){
         nW = nW + 50000;
         hgn = hgn - 30;
         sP = sP +5;
+        jam = jam + 4;
         printf("Hilmy menjadi budak korporat.\n");
     }
     else if(act=='F'){
@@ -50,12 +56,14 @@ int aksi(char act){
         nW = nW + 20000;
         hgn = hgn - 30;
         sP = sP +7;
+        jam = jam + 3;
         printf("Hilmy menjadi budak danusan.\n");
     }
     else if(act=='G'){
         hP = hP + 10;
         fP = fP + 15;
         hgn = hgn - 18;
+        jam = jam + 1;
         printf("Hilmy bersepeda.\n");
     }
     else if(act=='H'){
@@ -64,6 +72,7 @@ int aksi(char act){
         sP = sP - 40;
         fP = fP - 30;
         hgn = hgn - 40;
+        jam = jam + 6;
         printf("Hilmy melatih teknik pernapasan buatannya sendiri 'sanso no kokyuu'.\n");
     }
     else if(act=='I'){
@@ -71,6 +80,7 @@ int aksi(char act){
         sP = sP + 25;
         hgn = hgn - 15;
         nW = nW - 20000;
+        jam = jam + 3;
         printf("Hilmy bertemu dgn temannya Terminator dan CZ2I28 Delta.\n");
     }
     else if(act=='J'){
@@ -78,6 +88,7 @@ int aksi(char act){
         hP = hP + 8;
         nW = nW - 20000;
         hgn = hgn - 15;
+        jam = jam + 3;
         printf("Hilmy menghabiskan Rp 20000 hanya demi menambah 25 poin happiness.\n");
     }
     else if(act=='K'){
@@ -85,20 +96,23 @@ int aksi(char act){
         nW = nW - 5000;
         hgn = hgn - 5;
         sP = sP + 10;
+        jam = jam + 2;
         printf("Hilmy menghabiskan kuota seharga 5000 utk ngechat Do'i.\n");
     }
     else if(act=='L'){
         hP = 0;
-        printf("Hilmy masuk neraka.\n");
+        printf("Hilmy tidak tahan stress/tekanan dari kehidupan. Semua komplain silahkan disalurkan ke divisi mekanik.\n");
     }
     else if(act=='M'){
         hgn = hgn + 25;
         nW = nW -5000;
+        jam = jam + 1;
         printf("Hilmy cuci badan.\n");
     }
     else {
         hgn = hgn + 15;
         nW = nW - 4000;
+        jam = jam + 1;
         printf("Hilmy membuang ampas.\n");
         }
     return 0;
@@ -111,7 +125,7 @@ void display_stat() {
     printf("A Makan         \t E Kerja Part-time    \t I Hang out wth frens\t M. Mandi\n");
     printf("B Tidur         \t F Jualan Ekado       \t J Jalan-jalan       \t N. Buang Air\n");
     printf("C Maraton Anime \t G Main Sepeda        \t K Ngabari Do'i\n");
-    printf("D Ibadah        \t H Latihan Pernapasan \t L Commit to Suicide \n");
+    printf("D Ibadah        \t H Latihan Pernapasan \t L Commit Suicide \n");
 }
 
 int main()
@@ -120,10 +134,55 @@ int main()
     printf("Hari ini anda akan berberan sebagai: \t  Hilmy\n");
     printf("Seorang jomblo sejati yang berjuang untuk 'survive' \nTerlahir di lingkungan pembunuh\nDan sedang mencari jati diri\n+=-=-=-=-=-=-=-=-=-=\n\n");
     while(hP > 0){
+        miskin = 1;
         display_stat();
-        printf("Masukan tindakan :");
-        scanf(" %c", &tindakan);
+
+        while (miskin == 1){
+            printf("Masukan tindakan :");
+            scanf(" %c", &tindakan);
+
+            if (nW >= 0) {
+                miskin = 0;
+            }
+            else {
+                printf("\nUang tidak cukup.\n ");
+            }
+        }
         aksi(tindakan);
+        if (xP >= 100) {
+            xP = xP-100;
+            lvl = lvl + 1;
+            printf("Hilmy telah melatih pernafasan cukup banyak, sehingga menemukan jurus baru: sanso no kokyuu, %d no kata. Semua status akan bertambah setiap ronde.\n", lvl);
+        }
+
+        hP = hP + lvl;
+        sP = sP + lvl;
+        fP = fP + lvl;
+        hgn = hgn + lvl;
+        nW = nW - 5000;
+        hP = hP - 3;
+
+        if (fP <= 0) {
+            hP = hP - 35;
+            sP = sP - 10;
+            hgn = hgn - 35;
+            nW = nW - 15000;
+            printf("Karena happiness terlalu rendah, Hilmy mulai depresi. Dia kehilangan motivasi untuk melakukan segala hal, seperti bersosialisasi, serta menjalani\n");
+            printf("kegiatan secara normal. Dia jg akan kehilangan self control dan akan menghabiskan lebih banyak uang.");
+        }
+        if (sP <= 0) {
+            fP = fP -45;
+            nW = nW - 15000;
+            printf("Karena social terlalu rendah, Hilmy akan sedih, serta akan menghabiskan uang untuk mengisi kekosongan di hati logamnya.");
+        }
+        if (hgn <= 0) {
+            fP = fP -15;
+            hP = hP - 35;
+            sP = sP - 25;
+            printf("Karena hygiene terlalu rendah, Hilmy akan dijauhi orang - dan sebagai akibat Hilmuy akan sedih - serta kesehatannya akan menurun.");
+        }
     }
+    printf("\n\n Hilmy telah meninggal dunia, setelah survive selama %d jam di dunia yg kejam ini.\n", jam);
+    printf("\n---------------- G A M E   O V E R -------------------");
     return 0;
 }
